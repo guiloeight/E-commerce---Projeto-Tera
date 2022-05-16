e6a82358cd69ebbdae013ba224fc7e9f06fe5b73
guiloeight <guiloeight@gmail.com>
2022-05-16 19:44:03 -0300

    v1

--- js/main.js
+++ js/main.js
@@ -1,51 +1,172 @@
+var list = [
+	{"desc":"rice", "amount":"1", "value":"5.40"},
+	{"desc":"beer", "amount":"12", "value":"1.99"},
+	{"desc":"meat", "amount":"1", "value":"15.00"}
+];
 
-console.log("Olá mundo!")
-// Conectar informações do botão desenvolvido no arquivo HTML
 
-let botao = document.getElementById("botao-topo")
-// Função nativa do windows chamada onscroll ao scrollar, armazena a função chamada scrollFunction(), que define quando o botão deve aparecer
-window.onscroll = function scrollFunction () {
-  // Se, a quantidade de pixels percorridos na tela for igual à condição passada entre parentêses, então:
-  if (document.body.scrollTop > 180 || document.documentElement.scrollTop > 180) {
-    // mostrar o botão
-    botao.style.display = "block";
-    // senão,
-  } else {
-    // não mostrar o botão 
-    botao.style.display = "none";
-  }
+//somando total
+function getTotal(list){
+	var total = 0;
+	for(var key in list){
+		total += list[key].value * list[key].amount;
+	}
+	document.getElementById("totalValue").innerHTML = formatValue(total);
 }
 
-// Quando a pessoa clicar em voltar para o topo, executar esta função
-function voltarParaTopo () {
-    document.documentElement.scrollTop = 0;
+//criando a tabela
+function setList(list){
+	var table = '<thead><tr><td>Description</td><td>Amount</td><td>Value</td><td>Action</td></tr></thead><tbody>';
+	for(var key in list){
+		table += '<tr><td>'+ formatDesc(list[key].desc) +'</td><td>'+ formatAmount(list[key].amount) +'</td><td>'+ formatValue(list[key].value) +'</td><td><button class="btn btn-default" onclick="setUpdate('+key+');">Edit</button> <button class="btn btn-default" onclick="deleteData('+key+');">Delete</button></td></tr>';
+	}
+	table += '</tbody>';
+
+	document.getElementById('listTable').innerHTML = table;
+	getTotal(list);
+	saveListStorage(list);
 }
 
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+//formatando o nome do produto
+function formatDesc(desc){
+	var str = desc.toLowerCase();
+	str = str.charAt(0).toUpperCase() + str.slice(1);
+	return str;
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+//formatando a quantidade
+function formatAmount(amount){
+	return parseInt(amount);
 }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+//formatando o preço
+function formatValue(value){
+	var str = parseFloat(value).toFixed(2) + "";
+	str = str.replace(".",",");
+	str = "$ " + str;
+	return str;
 }
+
+//adicionar novo produto
+function addData(){
+	if(!validation()){
+		return;
+	}
+	var desc = document.getElementById("desc").value;
+	var amount = document.getElementById("amount").value;
+	var value = document.getElementById("value").value;
+
+	list.unshift({"desc":desc , "amount":amount , "value":value });
+	setList(list);
+}
+
+//botões de editar
+function setUpdate(id){
+	var obj = list[id];
+	document.getElementById("desc").value = obj.desc;
+	document.getElementById("amount").value = obj.amount;
+	document.getElementById("value").value = obj.value;
+	document.getElementById("btnUpdate").style.display = "inline-block";
+	document.getElementById("btnAdd").style.display = "none";
+
+	document.getElementById("inputIDUpdate").innerHTML = '<input id="idUpdate" type="hidden" value="'+id+'">';
+}
+
+//limpa os campos de editar
+function resetForm(){
+	document.getElementById("desc").value = "";
+	document.getElementById("amount").value = "";
+	document.getElementById("value").value = "";
+	document.getElementById("btnUpdate").style.display = "none";
+	document.getElementById("btnAdd").style.display = "inline-block";
+	
+	document.getElementById("inputIDUpdate").innerHTML = "";
+	document.getElementById("errors").style.display = "none";
+}
+
+//atualizando os dados
+function updateData(){
+	if(!validation()){
+		return;
+	}
+	var id = document.getElementById("idUpdate").value;
+	var desc = document.getElementById("desc").value;
+	var amount = document.getElementById("amount").value;
+	var value = document.getElementById("value").value;
+
+	list[id] = {"desc":desc, "amount":amount, "value":value};
+	resetForm();
+	setList(list);
+}
+
+//deletando os dados
+function deleteData(id){
+	if(confirm("Delete this item?")){
+		if(id === list.length - 1){
+			list.pop();
+		}else if(id === 0){
+			list.shift();
+		}else{
+			var arrAuxIni = list.slice(0,id);
+			var arrAuxEnd = list.slice(id + 1);
+			list = arrAuxIni.concat(arrAuxEnd);
+		}
+		setList(list);
+	}
+}
+
+//validando e printando erros
+function validation(){
+	var desc = document.getElementById("desc").value;
+	var amount = document.getElementById("amount").value;
+	var value = document.getElementById("value").value;
+	var errors = "";
+	document.getElementById("errors").style.display = "none";
+
+	if(desc === ""){
+		errors += '<p>Fill out description</p>';
+	}
+	if(amount === ""){
+		errors += '<p>Fill out a quantity</p>';
+	}else if(amount != parseInt(amount)){
+		errors += '<p>Fill out a valid amount</p>';
+	}
+	if(value === ""){
+		errors += '<p>Fill out a value</p>';
+	}else if(value != parseFloat(value)){
+		errors += '<p>Fill out a valid value</p>';
+	}
+
+	if(errors != ""){
+		document.getElementById("errors").style.display = "block";
+		document.getElementById("errors").innerHTML = "<h3>Error:</h3>" + errors;
+		return 0;
+	}else{
+		return 1;
+	}
+}
+
+//deletando lista
+function deleteList(){
+	if (confirm("Delete this list?")){
+		list = [];
+		setList(list);
+	}
+}
+
+//salvando em storage
+function saveListStorage(list){
+	var jsonStr = JSON.stringify(list);
+	localStorage.setItem("list",jsonStr);
+}
+
+//verifica se já tem algo salvo
+function initListStorage(){
+	var testList = localStorage.getItem("list");
+	if(testList){
+		list = JSON.parse(testList);
+	}
+	setList(list);
+}
+
+initListStorage();
